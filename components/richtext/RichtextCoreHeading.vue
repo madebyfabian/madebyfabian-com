8bae336934e1be594107d44c8a8f1e61b641ba30
madebyfabian <beer.fabian@mail.de>
2023-01-24 19:34:01 +0100

    fix: add align option

--- components/richtext/RichtextCoreHeading.vue
+++ components/richtext/RichtextCoreHeading.vue
@@ -1,8 +1,28 @@
 <template>
-	<h1 class="RichtextCoreHeading" v-if="props.block.level === 1" v-html="props.block.content" :id="id" />
-	<h2 class="RichtextCoreHeading" v-else-if="props.block.level === 2" v-html="props.block.content" :id="id" />
-	<h3 class="RichtextCoreHeading" v-else-if="props.block.level === 3" v-html="props.block.content" :id="id" />
-	<h4 class="RichtextCoreHeading" v-else-if="props.block.level === 4" v-html="props.block.content" :id="id" />
+	<h1
+		class="RichtextCoreHeading"
+		:class="alignClass"
+		v-if="props.block.level === 1"
+		v-html="props.block.content"
+		:id="id" />
+	<h2
+		class="RichtextCoreHeading"
+		:class="alignClass"
+		v-else-if="props.block.level === 2"
+		v-html="props.block.content"
+		:id="id" />
+	<h3
+		class="RichtextCoreHeading"
+		:class="alignClass"
+		v-else-if="props.block.level === 3"
+		v-html="props.block.content"
+		:id="id" />
+	<h4
+		class="RichtextCoreHeading"
+		:class="alignClass"
+		v-else-if="props.block.level === 4"
+		v-html="props.block.content"
+		:id="id" />
 </template>
 
 <script setup lang="ts">
@@ -11,9 +31,23 @@
 			content: string
 			level: number
 			anchor?: string
+			textAlign?: string
 		}
 		innerBlocks: any[]
 	}>()
+
+	const alignClass = computed(() => {
+		switch (props.block.textAlign) {
+			case 'left':
+				return 'text-left'
+			case 'center':
+				return 'text-center'
+			case 'right':
+				return 'text-right'
+			default:
+				return ''
+		}
+	})
 
 	const id = computed(() => props.block.anchor || undefined)
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
 	<div class="container max-w-3xl">
-		<h1 class="text-center">{{ data.page?.title }}</h1>
 		<RichtextContainer :blocksRaw="data?.page?.blocks" slugKey="/" />
 		<LayoutHome class="mt-12" />
 	</div>
