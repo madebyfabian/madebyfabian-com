db78fbe02ff271d71cecf06297fb76fb37025e03
madebyfabian <beer.fabian@mail.de>
2023-01-24 19:16:21 +0100

    fix: improve footer

--- components/layout/LayoutFooter.vue
+++ components/layout/LayoutFooter.vue
@@ -1,8 +1,13 @@
 <template>
 	<footer class="border-t mt-16 py-8">
 		<div class="container flex justify-between items-center h-full">
-			&copy; {{ new Date().getFullYear() }}
-			<UILink to="https://madebyfabian.com">madebyfabian.com</UILink>
+			<div class="font-bold">
+				&copy; {{ new Date().getFullYear() }} &mdash;
+				<UILink to="https://madebyfabian.com">madebyfabian.com</UILink>
+			</div>
+			<div class="flex gap-x-6 gap-y-3 flex-wrap">
+				<UILink to="/impressum">Impressum</UILink>
+			</div>
 		</div>
 	</footer>
 </template>
