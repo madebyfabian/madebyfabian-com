11847b1b28a0920971c74416878153dba8a62b9a
madebyfabian <beer.fabian@mail.de>
2023-01-26 15:29:23 +0100

    fix: debug again

--- server/api/debug.ts
+++ server/api/debug.ts
@@ -1,7 +1,21 @@
 export default defineEventHandler(event => {
 	const runtimeConfig = useRuntimeConfig()
+	const headers = getHeaders(event)
+
+	let host: string | undefined = ''
+	if (process.server) {
+		host = headers?.host
+	} else {
+		host = window.location.host
+	}
+
+	const url = host ? `https://${host}` : undefined
 
 	return {
+		url,
+		host,
 		runtimeConfig,
 	}
+
+	//return url === runtimeConfig.public.siteUrlPreview
 })
