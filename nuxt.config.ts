c3f50150c4d8f5eb1153a96b2de5009b19f26441
madebyfabian <beer.fabian@mail.de>
2023-01-27 13:19:25 +0100

    fix: adding vercel image route rule

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -61,8 +61,9 @@ export default defineNuxtConfig({
 		},
 	},
 
-	// Do not include the index route in the sitemap. (Because it would be duplicated),
 	routeRules: {
+		// Do not include the index route in the sitemap. (Because it would be duplicated),
 		'/': { index: false },
+		'/_vercel/image/**': { headers: { 'cache-control': `public, max-age=${60 * 60 * 24}` } },
 	},
 })
