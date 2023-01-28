2faa2e54f43f2b730cf29d4766249728f827be4a
madebyfabian <beer.fabian@mail.de>
2023-01-28 09:45:39 +0100

    fix: add `external prop to link`

--- components/layout/LayoutFooter.vue
+++ components/layout/LayoutFooter.vue
@@ -3,7 +3,7 @@
 		<div class="container flex justify-between items-center h-full">
 			<div class="font-bold">
 				&copy; {{ new Date().getFullYear() }} &mdash;
-				<UILink to="https://madebyfabian.com">madebyfabian.com</UILink>
+				<UILink to="https://madebyfabian.com" external>madebyfabian.com</UILink>
 			</div>
 
 			<LayoutMenu menuId="dGVybTo4" class="flex gap-x-6 gap-y-3 flex-wrap" />
