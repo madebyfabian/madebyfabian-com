484955af9b6a42c501e546ce4425bbd72dcdfd09
madebyfabian <beer.fabian@mail.de>
2023-01-29 14:47:07 +0100

    fix: improve link/button styles

--- components/layout/LayoutFooter.vue
+++ components/layout/LayoutFooter.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="LayoutFooter border-t mt-16 py-8">
 		<div class="container flex justify-between items-center h-full">
-			<div class="font-bold">
+			<div class="font-semibold">
 				&copy; {{ new Date().getFullYear() }} &mdash;
 				<NuxtLink class="UILink" to="https://madebyfabian.com" external>
 					<span>madebyfabian.com</span>
