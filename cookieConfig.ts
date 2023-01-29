8a17b8bb3286f1453e62667632d8d0f08592182c
madebyfabian <beer.fabian@mail.de>
2023-01-29 04:57:39 +0100

    fix: remove unneeded ponyfill

--- cookieConfig.ts
+++ cookieConfig.ts
@@ -17,6 +17,7 @@ export const cookieConfig: Partial<ModuleOptions> & {
 } = {
 	barPosition: 'bottom-right',
 	locales: [locale],
+	isCssPolyfillEnabled: false,
 	cookieExpiryOffsetMs: 1000 * 60 * 60 * 24 * 30, // 30 days,
 	cookies: {
 		necessary: [],
