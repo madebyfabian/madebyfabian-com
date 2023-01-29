33e43300223a63a7072965e0ed812216be4a5993
madebyfabian <beer.fabian@mail.de>
2023-01-29 05:09:37 +0100

    fix: finally enable cookie-control

--- composables/useIsCookiePermitted.ts
+++ composables/useIsCookiePermitted.ts
@@ -1,7 +1,6 @@
 import type { CookieId } from '@/cookieConfig'
 
 export const useIsCookiePermitted = (cookieId: CookieId) => {
-	/*const { cookiesEnabledIds } = useCookieControl()
-	return !!cookiesEnabledIds.value?.includes(cookieId)*/
-	return true
+	const { cookiesEnabledIds } = useCookieControl()
+	return !!cookiesEnabledIds.value?.includes(cookieId)
 }
