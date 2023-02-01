d0b36e314607901b0ec83f533f6f974d828f56d9
madebyfabian <hello@madebyfabian.com>
2023-02-01 22:33:33 +0100

    wip: first steps

---
+++ api/contact-form.ts
@@ -0,0 +1,56 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { GraphQLClient } from 'graphql-request'
+import { z } from 'zod'
+export { graphql as gql } from '@/types/gen/graphql'
+
+const graphQLClient = new GraphQLClient(process.env.NUXT_GQL_HOST || '', {
+	headers: {
+		authorization: process.env.NUXT_GQL_TOKEN || '',
+	},
+})
+
+const getContactFormEmail = async () => {
+	const query = gql(/* GraphQL */ `
+		query AdminEmail {
+			allSettings {
+				generalSettingsEmail
+			}
+		}
+	`)
+	return graphQLClient.request(query)
+}
+
+/**
+ * Todo:
+ * - [x] We want to verify the input into this request.
+ * - [ ] (We want to verify the turnstile token.)
+ * - [x] We want to fetch the admin email from wp graphql.
+ * - [ ] We want to render the email body.
+ * - [ ] We want to prepare and send the email via graphql.
+ */
+
+export default async function handler(request: VercelRequest, response: VercelResponse) {
+	try {
+		// First validate input
+		const schema = z.object({
+			name: z.string(),
+			email: z.string(),
+			message: z.string(),
+			turnstileToken: z.string(),
+		})
+
+		const contactFormEmail = await getContactFormEmail()
+
+		const input = schema.parse(request.body)
+
+		return response.status(200).json({
+			body: request.body,
+			input,
+			contactFormEmail,
+		})
+	} catch (error) {
+		return response.status(500).json({
+			error: (error as Error).message,
+		})
+	}
+}
