d2a88b485e897fd5274b032c82f91f5176ce9188
madebyfabian <hello@madebyfabian.com>
2023-02-01 17:22:50 +0100

    fix: correct syntax

--- components/BgBlur.vue
+++ components/BgBlur.vue
@@ -1,7 +1,7 @@
 <template>
 	<div aria-hidden="true" class="BgBlur overflow-hidden">
 		<div class="container fixed z-0 select-none h-[75vh] -top-[10vh] -left-[10vh] pointer-events-none">
-			<NuxtImg src="/images/blur.png" height="800" width="800" class="h-full w-auto" alt />
+			<NuxtImg src="/images/blur.png" height="800" width="800" class="h-full w-auto" alt="" />
 		</div>
 	</div>
 </template>
