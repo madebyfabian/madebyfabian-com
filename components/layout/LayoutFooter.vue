0214aa8c8563983513310e7099ff911c9fb89f2e
madebyfabian <hello@madebyfabian.com>
2023-02-02 01:18:23 +0100

    fix: layout

--- components/layout/LayoutFooter.vue
+++ components/layout/LayoutFooter.vue
@@ -1,6 +1,6 @@
 <template>
 	<footer class="LayoutFooter border-t mt-16 py-8">
-		<div class="container flex justify-between items-center h-full">
+		<div class="container flex justify-between items-center flex-wrap gap-6 h-full">
 			<div class="font-semibold">
 				&copy; {{ new Date().getFullYear() }} &mdash;
 				<NuxtLink class="UILink" to="https://madebyfabian.com" external>
