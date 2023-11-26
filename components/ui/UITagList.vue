94e00cd7a785c76e891b97845d3772f6d4334ce7
madebyfabian <hello@madebyfabian.com>
2023-11-26 14:43:39 +0100

    fix: link and tag list appearance

--- components/ui/UITagList.vue
+++ components/ui/UITagList.vue
@@ -1,6 +1,11 @@
 <template>
-	<ul v-if="props.tags?.length" class="UITagList gap-x-2.5 gap-y-1.5 flex flex-wrap">
-		<li v-for="tag of props.tags" :key="tag.node.id" class="text-gray-500 underline"># {{ tag.node.name }}</li>
+	<ul v-if="props.tags?.length" class="UITagList gap-x-2 gap-y-2 flex flex-wrap">
+		<li
+			v-for="tag of props.tags"
+			:key="tag.node.id"
+			class="text-gray-500 text-sm px-3 py-1 rounded-full shadow-border font-medium bg-frost-200">
+			{{ tag.node.name }}
+		</li>
 	</ul>
 </template>
 
